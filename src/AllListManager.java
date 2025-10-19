7542f09ba84aa6105d3868fd902e7d81d27261c0
Everton-lf <everton.lucas@aluno.ufca.edu.br>
2025-10-19 09:53:57 -0300

    Completion task 2

--- src/AllListManager.java
+++ src/AllListManager.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -11,94 +12,89 @@ public class AllListManager {
         int option=0;
 
         while (option !=4) {
+            showMenu();
             try {
-                System.out.println("Pick a option\n");
-                System.out.println("1 - Add Taks");
-                System.out.println("2 - To view Taks");
-                System.out.println("3 - Mark Taks as Fineshed");
-                System.out.println("4 - Exit");
                 option = typed.nextInt();
+                typed.nextLine();
             } catch (InputMismatchException e) {
                 System.out.println("invalid option, pick 1 2 3 or 4");
                 typed.nextLine();
                 option=0;
                 continue;
             }
-
-            if( option > 4 || option < 1 ) {
-                System.out.println("Pick a valid option \n");
-                System.out.println("1 - Add Taks");
-                System.out.println("2 - To view Taks");
-                System.out.println("3 - Mark Taks as Fineshed");
-                System.out.println("4 - Exit");
-                option = typed.nextInt();
-            }else{
-                switch (option){
-                    case 1 :
-                        typed.nextLine();
-                        System.out.print("Include description :");
-                        String descp = typed.nextLine();
-
-                            System.out.print("\nIs Completed  Y/N ?");
-
-                            String cpl = typed.nextLine().toLowerCase();
-                            boolean completed = false;
-                            if(!cpl.isEmpty() && cpl.charAt(0)=='y'){
-                                completed = true;
-                            }
-
-                        Task newtask = new Task(descp, completed);
-                        tasks.add(newtask);
-
-                        System.out.println("✅ Task added successfully!");
-                        break;
-
-
-
-
-
-                    case 2 :
-                        for(Task seach : tasks){
-                            System.out.println("Name´s Task   :"+seach.getDescription()+" STATUS :"+ seach.isCompleted());
-
-                        }
-                        break;
-                    case 3 :
-                        for (int i= 0 ; i < tasks.size(); i++){
-                            Task tk = tasks.get(i);
-                            System.out.println("Task "+i+ " " + tk.getDescription() + " STATUS " +tk.isCompleted() );
-                        }
-                        System.out.println("Edit Taks STATUS !");
-                        int edit = typed.nextInt();
-                        Task edited = tasks.get(edit);
-                        if(edited.isCompleted()){
-                            edited.setCompleted(false);
-
-                        }else{
-                            edited.setCompleted(true);
-                        }
-                        System.out.println(edited.toString());
-
-
-                        break;
-                    case 4 :
-                        System.out.println("---------- EXIT ----------");
-
-                        break;
-
-
-
-                    default:
-                        throw new IllegalStateException("Unexpected value: " + option);
-                }
+            switch (option){
+                case 1 :
+                    addTasks(tasks,typed);
+                    break;
+                case 2 :
+                    viewTasks(tasks);
+                    break;
+                case 3 :
+                    markAsCompleted(tasks, typed);
+                    break;
+                case 4 :
+                    System.out.println("---------- EXIT ----------");
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + option);
             }
-
         }
-
-
-
-
         typed.close();
-
-        }
     }
+    private static void showMenu(){
+        System.out.println("Pick a option\n");
+        System.out.println("1 - Add Taks");
+        System.out.println("2 - To view Taks");
+        System.out.println("3 - Mark Taks as Fineshed");
+        System.out.println("4 - Exit");
+    }
+    private  static void addTasks(ArrayList<Task> tasks, Scanner scanner){
+        System.out.print("Include description :");
+        String descp = scanner.nextLine();
+        Task newtask = new Task(descp,false);
+        tasks.add(newtask);
+    }
+    private static void viewTasks(ArrayList<Task> tasks){
+        if(tasks.isEmpty()) {
+            System.out.println("\n The Tasks list is empty!");
+            return;
+        }
+        System.out.println("-------------Your Tasks-------------");
+        for (int i=0; i < tasks.size(); i++){
+            Task tasksview = tasks.get(i);
+            String status = tasksview.isCompleted() ? "[X]" : "[ ]";
+            System.out.printf("%d. %s %s%n", (i + 1), status, tasksview.getDescription());
+        }
+        System.out.println("------------------------------------");
+    }
+    private static void markAsCompleted(ArrayList<Task> tasks, Scanner typed){
+        if(tasks.isEmpty()) {
+            System.out.println("Don´t are there tasks to mark  ");
+        }
+        viewTasks(tasks);
+        System.out.println("Enter the task number to change");
+        int numbertask;
+        try {
+            numbertask = typed.nextInt();
+        }catch(InputMismatchException e){
+            System.out.println("X - Invalid entry, Please, enter number of list");
+            typed.nextLine();
+            return;
+        }
+        int index = numbertask -1;
+        if(index >=0 && index < tasks.size()) {
+            Task taskToEdit = tasks.get(index);
+            boolean newStatus = !taskToEdit.isCompleted();
+            taskToEdit.setCompleted(newStatus);
+            String statusTexto = newStatus ? "COMPLETED" : "PENDING";
+            System.out.println("Task " + taskToEdit + " marked as " + statusTexto + ".");
+            System.out.println("Details: " + taskToEdit.getDescription());
+        } else {
+            System.out.println("X - Invalid Task number. Please, enter number of list.");
+        }
+
+    }
+}
+
+
+
